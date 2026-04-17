76b33b022cc1dff37d801687bf5662fd69cc57a0
KWAFO NATHANIEL SNR <kwafo.nathaniel.snr@example.com>
2026-04-17 04:44:47 +0000

    fix: resolve Kotlin compile error by aligning namespaces to com.bankly.gh in MainApplication and MainActivity

--- mobile/android/app/src/main/java/com/bankly/gh/MainActivity.kt
+++ mobile/android/app/src/main/java/com/bankly/gh/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kudi
+package com.bankly.gh
 
 import android.os.Build
 import android.os.Bundle
--- mobile/android/app/src/main/java/com/bankly/gh/MainApplication.kt
+++ mobile/android/app/src/main/java/com/bankly/gh/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.kudi
+package com.bankly.gh
 
 import android.app.Application
 import android.content.res.Configuration
